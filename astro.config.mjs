6729dfe728a7a1f88cde07812b9ac4897f66e1af
Miguel Ângelo <3688770+CarpenterBug@users.noreply.github.com>
2025-05-04 23:02:37 +0300

    Add Astro Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+    site: 'https://www.madpiece.com',
+    integrations: [preact()],
+    adapter: netlify(),
 });
