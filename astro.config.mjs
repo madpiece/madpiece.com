8c35da42d2645a0bdfffbd9130937b443392719c
Miguel Ângelo <3688770+m10a-dev@users.noreply.github.com>
2025-09-02 15:42:31 +0300

    New design

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import netlify from '@astrojs/netlify';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.madpiece.com',
-    integrations: [preact()],
-    adapter: netlify(),
+  site: 'https://www.madpiece.com',
+  integrations: [preact()],
+  adapter: netlify(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
